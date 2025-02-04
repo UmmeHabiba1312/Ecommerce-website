4b940690fdc2d41600995c31621489750db7170d
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2025-02-04 21:48:28 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import CartProvider from "@/components/Provider";
 import { ToastContainer } from 'react-toastify'; // Import ToastContainer
 import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
+import Script from 'next/script'; // Import Script component from Next.js
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,15 +23,73 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Wrap everything inside CartProvider to share the cart state */}
+        
         <CartProvider>
           <Header />
           <main className="max-w-screen-2xl mx-auto">{children}</main>
           <Footer />
-          {/* Add ToastContainer here */}
+          
           <ToastContainer />
+         
+          <Script
+            src="//code.tidio.co/dqiyveddgadpjdnga3hztbwam9k21tld.js"
+            strategy="afterInteractive" // Load the script after the page becomes interactive
+          />
         </CartProvider>
       </body>
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
+// import CartProvider from "@/components/Provider";
+// import { ToastContainer } from 'react-toastify'; // Import ToastContainer
+// import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Comforty-By-UmmeHabiba",
+//   description: "Comforty-By-UmmeHabiba. Where comfort meets craftsmanship. Elevate your space with thoughtfully designed chairs that blend elegance, quality, and relaxation.",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         {/* Wrap everything inside CartProvider to share the cart state */}
+//         <CartProvider>
+//           <Header />
+//           <main className="max-w-screen-2xl mx-auto">{children}</main>
+//           <Footer />
+//           {/* Add ToastContainer here */}
+//           <ToastContainer />
+//         </CartProvider>
+//       </body>
+//     </html>
+//   );
+// }
